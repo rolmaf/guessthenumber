4b996d9e6e6fa913d16ab14a0fbc363e04c77bb2
rolmaf <61619288+rolmaf@users.noreply.github.com>
2020-04-26 17:17:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let secret_number = Math.ceil(Math.random()*10);
+let i = 0;
+document.querySelector(".all").onclick = function() {
+    if (event.target.classList.contains("number")) {
+        i++;
+        let this_number = event.target.innerHTML;
+        if (this_number == secret_number) {
+            document.querySelector("#finish").innerHTML = "Это правильно! Вы угадали! Это было число  " + this_number + "!";
+        }
+        if (this_number > secret_number) {
+            document.querySelector("#finish").innerHTML = "Секретное число меньше " + this_number;
+        }
+        if (this_number < secret_number) {
+            document.querySelector("#finish").innerHTML = "Секретное число больше " + this_number;
+        }
+        document.querySelector("#popytki").innerHTML = "Попытки: " + i + "!";
+    }
+}
+document.querySelector("#new-game").onclick = function() {
+    secret_number = Math.floor(Math.random()*10);
+    i = 0;
+    document.querySelector("#popytki").innerHTML = "Попытки: " + i + "!";
+    document.querySelector("#finish").innerHTML = "Как вы думаете, какое число загадал компьютер?";
+}
